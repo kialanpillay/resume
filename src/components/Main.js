ff89a8b9e3f321fd2b8bd960d33639e2b68d3ef3
Kialan Pillay <kialanpillay@gmail.com>
2020-04-10 00:27:02 +0200

    Contact Form Functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -215,7 +215,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action="contact.php" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
